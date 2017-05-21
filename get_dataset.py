e08c9539e3a5a09602883a06f309586b10d260e7
Arda Mavi <ardamavi2@gmail.com>
2017-05-21 20:48:39 +0300

    Shuffle datasets and encode label feature added

--- get_dataset.py
+++ get_dataset.py
@@ -5,6 +5,7 @@ from os import listdir
 from skimage import color, io
 from scipy.misc import imresize
 from sklearn.cross_validation import train_test_split
+from tflearn.data_utils import shuffle, to_categorical
 
 def get_img(data_path):
     # Getting image array from path:
@@ -25,4 +26,9 @@ def get_dataset(dataset_path='Data/Train_Data'):
             X.append(img)
             Y.append(label)
     X, X_test, Y, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42) # Create dateset
+    # Shuffle datas:
+    X, Y = shuffle(X, Y)
+    # Encode Y:
+    Y = to_categorical(Y, 2)
+    Y_test = to_categorical(Y_test, 2)
     return X, X_test, Y, Y_test
