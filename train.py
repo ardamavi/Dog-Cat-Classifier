7c9e96cc3e01641071e63d7fe94f30356b221478
Arda Mavi <ardamavi2@gmail.com>
2017-06-03 12:09:02 +0300

    Data generator updated

--- train.py
+++ train.py
@@ -6,12 +6,14 @@ def train_model(model, X, X_test, Y, Y_test):
 
     """
     # For better yield. The duration of the training is extended.
+
     from keras.preprocessing.image import ImageDataGenerator
     generated_data = ImageDataGenerator(featurewise_center=False, samplewise_center=False, featurewise_std_normalization=False, samplewise_std_normalization=False, zca_whitening=False, rotation_range=0,  width_shift_range=0.1, height_shift_range=0.1, horizontal_flip = True, vertical_flip = False)
     generated_data.fit(X)
-    model.fit_generator(generated_data.flow(X, Y, batch_size=30), steps_per_epoch=30, epochs=25, validation_data=(X_test, Y_test))
+    import numpy
+    model.fit_generator(generated_data.flow(X, Y, batch_size=10), steps_per_epoch=X.shape[0], epochs=25, validation_data=(X_test, Y_test))
     """
-    
+
     return model
 
 def main():
